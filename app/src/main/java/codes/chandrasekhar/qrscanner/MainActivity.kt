923ff9b414db78b5ef6706fc20749b4769e384d9
Chandra Sekhar <sekhar.chandra.5832@gmail.com>
2023-01-26 03:45:22 +0530

    Dependency added

--- app/src/main/java/codes/chandrasekhar/qrscanner/MainActivity.kt
+++ app/src/main/java/codes/chandrasekhar/qrscanner/MainActivity.kt
@@ -1,13 +1,38 @@
 package codes.chandrasekhar.qrscanner
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import codes.chandrasekhar.qrscanner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val permission : Array<String> = arrayOf(
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        android.Manifest.permission.CAMERA)
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        registerForPermission.launch(permission)
+        binding.generateQr.setOnClickListener{
+            startActivity(Intent(this@MainActivity, GenerateQR::class.java))
+        }
+    }
+    private val registerForPermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){ it ->
+        if(!it.entries.all { it.value }){
+            Toast.makeText(this@MainActivity, "Need permission to use this app", Toast.LENGTH_SHORT).show()
+            finishAffinity()
+        }
     }
 }
